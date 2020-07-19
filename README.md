3e31c9b9835d764f833d45ca45c9479c865a90b7
Caio Teixeira <caiovmteixeira@outlook.com>
2020-07-19 17:28:26 -0300

    Squashed commit of the following:
    commit 5e44fd65b4b5a3a5deebcf346ff3c82ab3f62752
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 17:26:37 2020 -0300
    
        Initial Code
    
    commit 761ebee3d9aebff5b5c6e5a6782c2adf8adead11
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 17:19:05 2020 -0300
    
        Remove messages
    
    commit 13abed204dbbb53030e58da9511c113602eaa487
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 17:08:35 2020 -0300
    
        Fix direct messages
    
    commit 41a3ea03dbdb823ba4e3e700a2d82542757d1373
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 17:05:02 2020 -0300
    
        Always answer if direct message
    
    commit 1ac3dbfb599fe185eb5fb840b154220337127c76
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 16:53:23 2020 -0300
    
        Always generate answers
    
    commit caddc7f5b6f6000546ec7af9a885e1eddcd3a9b8
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 16:49:14 2020 -0300
    
        Disable training messages
    
    commit 0f660b624ea783d9e611945f90287684c2b5a9ad
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 16:47:12 2020 -0300
    
        Random chance to answer
    
    commit c6b72b0facb1b0d5b34651f01d60f970bf8fe790
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 16:31:44 2020 -0300
    
        handle lists
    
    commit da2f8b6511da60f166c28e83e90267cf6e967c64
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 16:23:52 2020 -0300
    
        requirements
    
    commit fbf68d68d64b768870a5266252728f87c58da0c5
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 16:19:26 2020 -0300
    
        Remove corpus
    
    commit e2a7ca97cce7e03c9ce3db6385bdb71086dd55f4
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 16:16:48 2020 -0300
    
        Change pyyml requirement
    
    commit 3bbb2344f2018404c6c3b1d96f7e722a936518d6
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 16:08:16 2020 -0300
    
        Test
    
    commit 1a0bc0f9f7d9b729471a585b91cd0e2f11692461
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 15:49:18 2020 -0300
    
        Fix json load
    
    commit b20b4abd61c0f48fb7c28cfb39f2b8304c7118f6
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 12:48:46 2020 -0300
    
        Fix document parse
    
    commit 09d6cf483c47c6ab181867c4e6bb962566c92c47
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 12:45:48 2020 -0300
    
        Fix file trainer
    
    commit f2a05878521e71ee6d8065d3f78df4ca0046078a
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 12:40:52 2020 -0300
    
        Try to fix again
    
    commit 26cf1098c1cb6796b984f0f878b6bf53232e0286
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 12:36:24 2020 -0300
    
        delete webhook
    
    commit c06a32f7a03b93e3645090e8feec07e67401679f
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 12:32:01 2020 -0300
    
        Fixes
    
    commit 0153b4af9a337645ca160050139d67d1c0ed6f4e
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 12:20:45 2020 -0300
    
        Disable default trainer
    
    commit d68975a9e983a0640d99c70e02d0004dba8cd9e2
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 12:16:48 2020 -0300
    
        Add dependency
    
    commit 421702b1e901cdc4f7e85ea93b0d88839806b6b4
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 12:14:20 2020 -0300
    
        Change to postgres
    
    commit 3c4dff0c6b4652c23dd421b74149cb40a2faec04
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 12:04:08 2020 -0300
    
        disable retry writes
    
    commit 8dc52e5084e4f67332bec1991d63373c200592b9
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 11:58:37 2020 -0300
    
        Add corpus
    
    commit bf30b3e7993f66328f9051dd9bc7e3c326a4fec3
    Author: Caio Teixeira <caiovmteixeira@outlook.com>
    Date:   Sun Jul 19 11:51:54 2020 -0300
    
        Initial code

---
+++ README.md
@@ -0,0 +1,32 @@
+# Telegram Chat Bot
+
+#####  This is a simple telegram chat bot. It is based on [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot) and [chatterbot](https://github.com/gunthercox/ChatterBot).
+--
+
+#### Tested on Linux 64bit with Python 3.5
+--
+# Setup
+I'm assuming you have Python 3 installed and running on your PC.
+
+1- Get you Bot Access token by creating a new bot on [BotFather](http://t.me/BotFather).
+2- Add the bot token in chat_bot.py (Search for the commented line)
+3- Install the required libraries using these commands:
+#### On Linux
+>> sudo -H pip3 install -r requirements.txt 
+#### On Windows
+>> pip3 install -r requirements.txt
+
+If you like my work you can  [donate](https://cyberboysumanjay.github.io/donate.html). 
+Join my [Telegram Channel](https://t.me/sjprojects) for more updates on my works.
+# Fire up the script
+To run the script type
+>> python3 chat_bot.py
+
+Now have a cup of coffee and wait for the training of the bot to finish.
+When training is finished, start the bot
+
+##### Happy Chatting!
+--
+#### Any suggestion is welcomed :)
+
+More about [Sumanjay](https://cyberboysumanjay.github.io)
---
+++ chat_bot.py
@@ -0,0 +1,79 @@
+# Setting up Chatterbot
+import os
+import json
+import random
+
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+# Train the bot
+english_bot = ChatBot("English Bot",
+                      storage_adapter="chatterbot.storage.SQLStorageAdapter",
+                      database_uri=os.environ.get("DATABASE_URL"))
+
+# Setting telegram things
+tg_token = os.environ.get("BOT_TOKEN")
+
+import logging
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
+
+updater = Updater(token=tg_token)
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Olá :)")
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
+def reply(bot, update):
+    userText = str(update.message.text)
+    answer = str(english_bot.get_response(userText))
+
+    botName = bot.name
+
+    if random.random() <= 0.2 or botName in userText:
+        bot.send_message(chat_id=update.message.chat_id, text=answer)
+
+
+reply_handler = MessageHandler(Filters.text, reply)
+dispatcher.add_handler(reply_handler)
+
+
+def train(bot, update):
+    file = bot.getFile(update.message.document.file_id)
+    rawData = file.download_as_bytearray()
+    data = rawData.decode('utf8')
+    messages = json.loads(data)
+
+    train_data = []
+    list_trainer = ListTrainer(english_bot)
+
+    for message in messages["messages"]:
+        if message["text"] is str:
+            train_data.append(message["text"])
+        if len(train_data) > 100:
+            list_trainer.train(train_data)
+            train_data = []
+
+
+train_handler = MessageHandler(Filters.document, train)
+dispatcher.add_handler(train_handler)
+
+
+def setup_webhook(updater, token):
+    webhook = os.environ.get('WEBHOOK')
+    port = int(os.environ.get('PORT', '8443'))
+    updater.start_webhook(listen="0.0.0.0",
+                          port=port,
+                          url_path=token)
+    updater.bot.set_webhook(webhook + token)
+
+
+setup_webhook(updater, tg_token)
+updater.idle()
